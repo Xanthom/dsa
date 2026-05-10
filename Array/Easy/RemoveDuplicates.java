679adefa769afb0aee9ee7808bc94193a2df6688
vikneshd04 <148607386+vikneshd04@users.noreply.github.com>
2026-05-10 10:09:03 +0530

    first commit

---
+++ Array/Easy/RemoveDuplicates.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    ArrayList<Integer> removeDuplicates(int[] arr) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        
+        list.add(arr[0]);
+        
+        for(int i = 1; i < arr.length; i++){
+            
+            if(arr[i] != arr[i-1]){
+                
+                list.add(arr[i]);
+            }    
+        }
+        return list;
+    }
+}
---
+++ Array/Easy/SecondLargest.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int getSecondLargest(int[] arr) {
+        // code here
+        int largest = arr[0];
+        int secondLargest = -1;
+        
+        for(int i =0; i < arr.length; i++){
+            
+            if(arr[i] > largest){
+                
+                largest = arr[i];
+            }
+        }
+        
+        
+        for(int i = 0; i < arr.length; i++){
+            
+            if(arr[i] > secondLargest && arr[i]<largest){
+                secondLargest = arr[i];
+            }
+        }
+        return secondLargest;
+    }
+}
